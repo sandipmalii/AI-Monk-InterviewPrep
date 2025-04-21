f1c22142e7cef120c8c1114a2c02c7e7e2c099d6
sandipmalii <21121a0567@vidyanikethan.asia>
2025-04-21 11:54:45 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const App = () => {
+  return (
+    <div className="text-blue-800">
+      <Button>click me</Button>
+    </div>
+  );
+};
+
+export default App;
